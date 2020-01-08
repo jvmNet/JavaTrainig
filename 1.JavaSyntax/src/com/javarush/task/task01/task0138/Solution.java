224902a4ca9e6d066079787a67f88658b754fb4f
jvmNet <direct@javac.online>
2020-01-08 20:56:00 +0200

    Initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        System.out.println("Мы знаем, что ныне лежит на весах");
+        System.out.println("И что совершается ныне.");
+        System.out.println("Час мужества пробил на наших часах,");
+        System.out.println("И мужество нас не покинет.");
+    }
+}
